af8a9c3815573f07e921a078b4164b387e743386
Alexander Matthew Payne <alexpayne1997@gmail.com>
2020-02-14 13:12:58 -0500

    Add docstring to open_pdb

--- molecool/io/pdb.py
+++ molecool/io/pdb.py
@@ -6,6 +6,23 @@ import numpy as np
 
 
 def open_pdb(file_location):
+    """Read a pdb file
+
+    The pdb file must specify the atom elements in the last column, and follow
+    the conventions outlined in the PDB format specification.
+
+    Paramters
+    ---------
+    file_location : str
+        Path to the pdb file.
+
+    Returns
+    -------
+    symbols : np.array
+        Array of single character ATOM symbols
+    coords : np.array
+        Array of xyz float coordinates
+    """
     with open(file_location) as f:
         data = f.readlines()
 
@@ -15,7 +32,7 @@ def open_pdb(file_location):
         if 'ATOM' in line[0:6] or 'HETATM' in line[0:6]:
             symbols.append(line[76:79].strip())
             atom_coords = [float(x) for x in line[30:55].split()]
-            coordinates.append(coords)
+            coordinates.append(atom_coords)
 
     coords = np.array(coordinates)
     symbols = np.array(symbols)
