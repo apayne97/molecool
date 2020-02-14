f0811b4790b2e6fee9e5b02d5c8bd0dfca152e91
Alexander Matthew Payne <alexpayne1997@gmail.com>
2020-02-14 14:07:57 -0500

    run yapf on molecool

--- molecool/measure.py
+++ molecool/measure.py
@@ -30,12 +30,13 @@ def calculate_distance(rA, rB):
 
     return distance
 
+
 def calculate_angle(rA, rB, rC, degrees=False):
     # Calculate the angle between three points. Answer is given in radians by default, but can be given in degrees
     # by setting degrees=True
     AB = rB - rA
     BC = rB - rC
-    theta=np.arccos(np.dot(AB, BC)/(np.linalg.norm(AB)*np.linalg.norm(BC)))
+    theta = np.arccos(np.dot(AB, BC) / (np.linalg.norm(AB) * np.linalg.norm(BC)))
 
     if degrees:
         return np.degrees(theta)
